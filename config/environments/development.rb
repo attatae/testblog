38bbc63a7f88235b4cc91810a0957bb13490eeb7
Christopher de Alba <superototus@gmail.com>
2015-05-09 03:32:33 -0700

    Install Devise, tune for User, generate

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # After installing Devise, it apparently wanted me to write this:
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  # In production, :host should be set to the actual host of your application.
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
